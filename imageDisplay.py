644b2c755a9b48c9af3d3a39ebe92d5b8ea41919
jdpelletier <john.pelletier@ymail.com>
2020-05-01 11:15:12 -1000

    first working version, loses all pixel info

--- imageDisplay.py
+++ imageDisplay.py
@@ -6,11 +6,20 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 from astropy.visualization import astropy_mpl_style
 from matplotlib import colors
-from PIL import Image
+import PIL.Image as PILimage
+import math
+
+#import ktl
 
 plt.style.use(astropy_mpl_style)
 
+#curAngle = ktl.cache('dcs', 'ROTPOSN')
+#parAngle = ktl.cache('dcs', 'PARANG')
 
+#def rotAngle(current, para):
+#    cur = current.read()
+#    par = para.read()
+#    return par - cur
 
 def walkDirectory():
     directory = '.'
@@ -29,9 +38,11 @@ def scan(timeout, cachedFiles):
     if hasNewFiles:
         print("New File Detected!")
         filen = files[0]
-        waitForFileToBeUnlocked(filen, 1);
-        plotIm(filename=filen)
-        displayIm()
+        waitForFileToBeUnlocked(filen, 1)
+        fitsData = fits.getdata(filen, ext=0)
+        header = fits.getheader(filen)
+        newdata = rotate_clip(fitsData, 45, 942, 747)
+        writeFits(header, newdata)
         print("File closed")
     time.sleep(timeout)
     return cachedFiles
@@ -90,22 +101,86 @@ def stop_scan():
     sys.exit()
 
 
-def plotIm(filename):
-    image_data = fits.getdata(filename, ext=0)
-    plt.figure()
-    plt.imshow(image_data, cmap='gray')
-    plt.colorbar()
-    plt.savefig("TRICKFullFrame.jpeg")
-    plt.show()
+def rotate_clip(data_np, theta_deg, rotctr_x=None, rotctr_y=None,
+                out=None, logger=None):
+    """
+    Rotate numpy array `data_np` by `theta_deg` around rotation center
+    (rotctr_x, rotctr_y).  If the rotation center is omitted it defaults
+    to the center of the array.
+    No adjustment is done to the data array beforehand, so the result will
+    be clipped according to the size of the array (the output array will be
+    the same size as the input array).
+    """
 
+    # If there is no rotation, then we are done
+    if math.fmod(theta_deg, 360.0) == 0.0:
+        return data_np
 
-def displayIm():
-    im = Image.open("TRICKFullFrame.jpeg")
-    rotated = im.rotate(45)
-    transposed = im.transpose(Image.ROTATE_90)
-    im.show()
-    rotated.show()
-    transposed.show()
+    ht, wd = data_np.shape[:2]
+    dtype = data_np.dtype
+
+    if rotctr_x is None:
+        rotctr_x = wd // 2
+    if rotctr_y is None:
+        rotctr_y = ht // 2
+
+    #pil rot
+    img = PILimage.fromarray(data_np)
+    img_rot = img.rotate(theta_deg, resample=False, expand=False,
+                         center=(rotctr_x, rotctr_y))
+    newdata = np.array(img_rot, dtype=data_np.dtype)
+    new_ht, new_wd = newdata.shape[:2]
+    assert (wd == new_wd) and (ht == new_ht), \
+        Exception("rotated cutout is %dx%d original=%dx%d" % (
+            new_wd, new_ht, wd, ht))
+    '''
+    #numpy rot
+    yi, xi = np.mgrid[0:ht, 0:wd]
+    xi -= rotctr_x
+    yi -= rotctr_y
+    cos_t = np.cos(np.radians(theta_deg))
+    sin_t = np.sin(np.radians(theta_deg))
+
+    if have_numexpr:
+        ap = ne.evaluate("(xi * cos_t) - (yi * sin_t) + rotctr_x")
+        bp = ne.evaluate("(xi * sin_t) + (yi * cos_t) + rotctr_y")
+    else:
+        ap = (xi * cos_t) - (yi * sin_t) + rotctr_x
+        bp = (xi * sin_t) + (yi * cos_t) + rotctr_y
+
+    #ap = np.rint(ap).clip(0, wd-1).astype(np.int)
+    #bp = np.rint(bp).clip(0, ht-1).astype(np.int)
+    # Optomizations to reuse existing intermediate arrays
+    np.rint(ap, out=ap)
+    ap = ap.astype(np.int, copy=False)
+    ap.clip(0, wd - 1, out=ap)
+    np.rint(bp, out=bp)
+    bp = bp.astype(np.int, copy=False)
+    bp.clip(0, ht - 1, out=bp)
+
+    if out is not None:
+        out[:, :, ...] = data_np[bp, ap]
+        newdata = out
+    else:
+        newdata = data_np[bp, ap]
+        new_ht, new_wd = newdata.shape[:2]
+
+        assert (wd == new_wd) and (ht == new_ht), \
+            Exception("rotated cutout is %dx%d original=%dx%d" % (
+                new_wd, new_ht, wd, ht))
+    #end np rot
+    '''
+    return newdata
+
+def writeFits(headerinfo, image_data):
+    hdu = fits.PrimaryHDU()
+    hdu.data = image_data
+    hdu.header = headerinfo
+    try:
+        hdu.writeto('rotatedImage.fits')
+    except OSError:
+        os.remove('rotatedImage.fits')
+        hdu.writeto('rotatedImage.fits')
 
 
 #####
